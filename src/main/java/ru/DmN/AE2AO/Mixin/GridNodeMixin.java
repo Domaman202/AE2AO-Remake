78de0d09461435df479a55abf8fe40f91d6110dc
Domaman202 <vip.domaman@mail.ru>
2021-04-10 14:38:16 +0700

    optimization!

--- src/main/java/ru/DmN/AE2AO/Mixin/GridNodeMixin.java
+++ src/main/java/ru/DmN/AE2AO/Mixin/GridNodeMixin.java
@@ -18,12 +18,6 @@ import java.util.concurrent.CompletableFuture;
 public abstract class GridNodeMixin {
     @Shadow private int usedChannels;
 
-//    @Shadow protected abstract void visitorConnection(Object t, IGridVisitor g, Deque<GridNode> nr, Deque<IGridConnection> nc);
-//    @Shadow protected abstract void visitorNode(final Object t, final IGridVisitor g, final Deque<GridNode> nr);
-
-//    private boolean f1 = true;
-//    private boolean f2 = true;
-
     /**
      * @author DomamaN202
      * @reason Add system turn off channels
@@ -32,34 +26,4 @@ public abstract class GridNodeMixin {
     {
         return Main.lc.DisableChannels ? 1 : usedChannels;
     }
-
-//    @Inject(method = "visitorConnection", at = @At(value = "HEAD", shift = At.Shift.AFTER))
-//    private void visitorConnection(Object t, IGridVisitor g, Deque<GridNode> nr, Deque<IGridConnection> nc, CallbackInfo c) {
-//        if (f1) {
-//            CompletableFuture.runAsync(() -> {
-//                try {
-//                    Thread.sleep(100);
-//                    visitorConnection(t, g, nr, nc);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//        } else
-//            f1 = true;
-//    }
-//
-//    @Inject(method = "visitorNode", at = @At(value = "HEAD", shift = At.Shift.AFTER))
-//    private void visitorNode(Object t, IGridVisitor g, Deque<GridNode> n, CallbackInfo c) {
-//        if (f2) {
-//            CompletableFuture.runAsync(() -> {
-//                try {
-//                    Thread.sleep(100);
-//                    visitorNode(t, g, n);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//        } else
-//            f2 = true;
-//    }
 }
--- src/main/java/ru/DmN/AE2AO/Mixin/PathGridCacheMixin.java
+++ src/main/java/ru/DmN/AE2AO/Mixin/PathGridCacheMixin.java
@@ -34,55 +34,22 @@ public abstract class PathGridCacheMixin {
         if (controllers.isEmpty()) {
             controllerState = ControllerState.NO_CONTROLLER;
         } else {
-//            ControllerBlockEntity[] controllers_ = controllers.toArray();
-            ArrayList<ControllerBlockEntity> controllers_ = new ArrayList<>(controllers);
-            ArrayList<ArrayList<ControllerBlockEntity>> controllerMap = new ArrayList<>();
-
-            controllerMap.add(new ArrayList<>());
-            controllerMap.get(0).add(controllers_.get(0));
-
-            for (int i = 1; i < controllers_.size(); i++) {
-                ControllerBlockEntity controller = controllers_.get(i);
-                boolean complete = false;
-
-                for (ArrayList<ControllerBlockEntity> map : controllerMap) {
-                    if (complete == true)
-                        break;
-                    for (ControllerBlockEntity c : map) {
-                        if (validate(controller.getPos(), c.getPos())) {
-                            complete = true;
-                            map.add(controller);
-                            break;
-                        }
-                    }
-                }
-
-                if (complete)
-                    continue;
-
-                ArrayList<ControllerBlockEntity> nmap = new ArrayList<>();
-                nmap.add(controller);
-                controllerMap.add(nmap);
-            }
-
             boolean valid = true;
 
-            for (ArrayList<ControllerBlockEntity> map : controllerMap) {
-                for (ControllerBlockEntity controller : map) {
-                    final IGridNode node = controller.getGridNode(AEPartLocation.INTERNAL);
-                    if (node == null) {
-                        this.controllerState = ControllerState.CONTROLLER_CONFLICT;
-                        return;
-                    }
-
-                    final DimensionalCoord dc = node.getGridBlock().getLocation();
-                    final ControllerValidator cv = new ControllerValidator(dc.x, dc.y, dc.z);
-
-                    node.beginVisit(cv);
-
-                    if (!cv.isValid())
-                        valid = false;
+            for (ControllerBlockEntity controller : controllers) {
+                final IGridNode node = controller.getGridNode(AEPartLocation.INTERNAL);
+                if (node == null) {
+                    this.controllerState = ControllerState.CONTROLLER_CONFLICT;
+                    return;
                 }
+
+                final DimensionalCoord dc = node.getGridBlock().getLocation();
+                final ControllerValidator cv = new ControllerValidator(dc.x, dc.y, dc.z);
+
+                node.beginVisit(cv);
+
+                if (!cv.isValid())
+                    valid = false;
             }
 
             if (valid)
