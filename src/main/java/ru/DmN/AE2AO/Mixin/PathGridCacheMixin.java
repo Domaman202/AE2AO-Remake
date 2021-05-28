80022ba0d49a03cf3ba4ade81241324bb2e55a5c
Domaman202 <vip.domaman@mail.ru>
2021-05-28 07:11:03 +0700

    Update PathGridCacheMixin.java

--- src/main/java/ru/DmN/AE2AO/Mixin/PathGridCacheMixin.java
+++ src/main/java/ru/DmN/AE2AO/Mixin/PathGridCacheMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import ru.DmN.AE2AO.Main;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 @Mixin(value = PathGridCache.class, remap = false)
