0c91e1462cfda8ee2e5b0108c8b000df573b0aa0
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-24 17:09:48 +0700

    hm..

--- src/main/java/ru/DmN/AE2AO/Mixin/AbstractStorageCellMixin.java
+++ src/main/java/ru/DmN/AE2AO/Mixin/AbstractStorageCellMixin.java
@@ -3,15 +3,39 @@ package ru.DmN.AE2AO.Mixin;
 import appeng.items.AEBaseItem;
 import appeng.items.storage.AbstractStorageCell;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import ru.DmN.AE2AO.Main;
 
 @Mixin(AbstractStorageCell.class)
 public class AbstractStorageCellMixin extends AEBaseItem {
+    @Shadow @Final protected int totalBytes;
+
     private AbstractStorageCellMixin(Settings p) { super(p); }
 
     @Override
     public boolean damage(DamageSource s) {
         return !Main.LC.SCFD && super.damage(s);
+    }
+
+    /**
+     * @author DomamaN202
+     * @reason Config
+     */
+    @Overwrite
+    public int getTotalTypes(final ItemStack cellItem) {
+        return Main.LC.StorageCellLimits ? 63 : Integer.MAX_VALUE;
+    }
+
+    /**
+     * @author DomamaN202
+     * @reason Config
+     */
+    @Overwrite
+    public int getBytes(ItemStack cellItem) {
+        return Main.LC.StorageCellLimits ? this.totalBytes : Integer.MAX_VALUE;
     }
 }
